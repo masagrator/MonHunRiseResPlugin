9eeca8ff8fd8a8d0c91342826b397e1345840515
MasaGratoR <masagrator@gmail.com>
2021-08-01 19:07:19 +0200

    Add pause

--- Patch.cmd
+++ Patch.cmd
@@ -3,3 +3,4 @@ nsnsotool 0100B04011742000\exefs\subsdk9 0100B04011742000\exefs\subsdk9_unc
 python MHRiseResolution.py
 nsnsotool 0100B04011742000\exefs\subsdk9_unc 0100B04011742000\exefs\subsdk9
 del 0100B04011742000\exefs\subsdk9_unc
+pause
